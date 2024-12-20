0aa0b00a2d4a604114e58b623ec6a9a09a2c8f4d
LiamSo121 <liam121300@gmail.com>
2024-12-20 18:44:27 +0200

    addprofilemodel

--- SoccerPedia/App/models.py
+++ SoccerPedia/App/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
 class UploadedImage(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +9,13 @@ class UploadedImage(models.Model):
     def __str__(self):
         return self.title
 
+
+
+# Create your models here.
+class Profile(models.Model):
+    additional_info = models.TextField(blank=True,null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null=True)
+
+
+    def __str__(self):
+        return str(self.id)
